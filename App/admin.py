b230566b3ac7fae88f3d3634298970f980c3d4c1
Rishi <rishimehra0478@gmail.com>
2022-08-16 10:17:01 +0530

    commit

--- App/admin.py
+++ App/admin.py
@@ -3,4 +3,4 @@ from App.models import Profile
 # Register your models here.
 @admin.register(Profile)
 class Page_Admin(admin.ModelAdmin):
-    list_display: ['user','auth_token','is_active','created_at']
+    list_display = ['user','auth_token','is_active','created_at']
